69d0972f796b928d82d3f29736f7e4421f6985c9
faun3 <antonioculcea@gmail.com>
2023-11-10 14:49:52 +0200

    show message on screen

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+class Parser {
+private:
+    static string cmdOptions;
+public:
+    void showMenu() {
+        cout << cmdOptions;
+    }
+};
+
+class Ingredient {};
+
+class Order {};
+
+string Parser::cmdOptions = "\nWelcome!\nYou can look at the menu using \"menu\".\nCheck out our currently available ingredients using \"stock\".\nAdd items to your order with \"order add <item name> <item quanitity>\".\nSee your current order using \"order show\".\nShow this screen again by typing the \"help\" command.\n\n";
+
 int main() 
 {
-    cout << "wow\n";
+    Parser cmdLineParser;
+    cmdLineParser.showMenu();
     return 0;
 }
