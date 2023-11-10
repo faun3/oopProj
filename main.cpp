fc677ebc20ba0b42bc4ab2c84b5e0c75dfa317e0
faun3 <antonioculcea@gmail.com>
2023-11-10 15:12:53 +0200

    add basic parsing functionality

--- main.cpp
+++ main.cpp
@@ -4,9 +4,23 @@ using namespace std;
 class Parser {
 private:
     static string cmdOptions;
+    bool running = true;
 public:
+    bool isRunning () {return running;}
     void showMenu() {
         cout << cmdOptions;
+    }
+    void parseLine() {
+        string userInput;
+        getline(cin, userInput);
+        string firstWord = userInput.substr(0, userInput.find(" "));
+        if (firstWord == "help") {
+            showMenu();   
+        }
+        else if (firstWord == "quit") {
+            running = false;
+        }
+        // cout << "\n" << firstWord << '\n';
     }
 };
 
@@ -14,11 +28,14 @@ class Ingredient {};
 
 class Order {};
 
-string Parser::cmdOptions = "\nWelcome!\nYou can look at the menu using \"menu\".\nCheck out our currently available ingredients using \"stock\".\nAdd items to your order with \"order add <item name> <item quanitity>\".\nSee your current order using \"order show\".\nShow this screen again by typing the \"help\" command.\n\n";
+string Parser::cmdOptions = "\nWelcome!\nYou can look at the menu using \"menu\".\nCheck out our currently available ingredients using \"stock\".\nAdd items to your order with \"order add <item name> <item quanitity>\".\nSee your current order using \"order show\".\nShow this screen again by typing the \"help\" command.\nYou can leave the restaurant using the \"quit\" command.\n\n";
 
 int main() 
 {
     Parser cmdLineParser;
     cmdLineParser.showMenu();
+    while (cmdLineParser.isRunning()) {
+        cmdLineParser.parseLine();
+    }
     return 0;
 }
