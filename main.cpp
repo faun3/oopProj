c92bac59f9c28722849e50a20cb2ef51b586e323
faun3 <antonioculcea@gmail.com>
2023-11-12 19:22:47 +0200

    more stuff again

--- main.cpp
+++ main.cpp
@@ -45,6 +45,17 @@ public:
     const char* getName() {
         return this->name;
     }
+    int getQty() {
+        return this->qty;
+    }
+    
+    void setQty(int newQty) {
+        if (newQty > 0) {
+            this->qty = newQty;
+        }
+        // throw something about negative quanitites here
+        else throw new exception;
+    }
 
     friend ostream& operator<<(ostream& ost, Ingredient& ing) {
         ost << "\n----------\n";
@@ -57,16 +68,33 @@ public:
 class MenuItem {
 private:
     char* menuItemName = nullptr;
-    typedef struct neccesaries {
-        Ingredient ingreedient;
-        int neceesaryQty;   
-    };
-    neccesaries needs[15];
-    int needsLenght = 0;
+    Ingredient* ingredientsNeeded = nullptr;
+    int ingredientsNeededLen = 0;
 public:
     MenuItem() {}
-    MenuItem() {
-
+    MenuItem(char* name, Ingredient* ingNeeded, int ingNeededLen) {
+        this->menuItemName = new char[strlen(name) + 1];
+        strcpy(this->menuItemName, name);
+        if (ingNeeded != nullptr && ingNeededLen > 0) {
+            this->ingredientsNeeded = new Ingredient[ingNeededLen];
+            this->ingredientsNeededLen = ingNeededLen;
+            for (int i = 0; i < this->ingredientsNeededLen; i++) {
+                this->ingredientsNeeded[i] = ingNeeded[i];
+            }
+        }
+    }
+    MenuItem(const MenuItem& menuItm) {
+        if (menuItm.menuItemName != nullptr) {
+            this->menuItemName = new char[strlen(menuItm.menuItemName) + 1];
+            strcpy(this->menuItemName, menuItm.menuItemName);
+        }
+        if (menuItm.ingredientsNeeded != nullptr && menuItm.ingredientsNeededLen > 0) {
+            this->ingredientsNeededLen = menuItm.ingredientsNeededLen;
+            this->ingredientsNeeded = new Ingredient[this->ingredientsNeededLen];
+            for (int i = 0; i < this->ingredientsNeededLen; i++) {
+                this->ingredientsNeeded[i] = menuItm.ingredientsNeeded[i];
+            }
+        }
     }
     void printItemAndNecessaries() {}
     friend ostream& operator<<(ostream& ost, MenuItem mi) {
@@ -83,8 +111,8 @@ public:
 
 class Menu {
 private:
-    MenuItem* menuItems;
-    int menuLength;
+    MenuItem* menuItems = nullptr;
+    int menuLength = 0;
 public:
     friend ostream& operator<<(ostream& ost, Menu menu){
         if (menu.menuItems != nullptr && menu.menuLength != 0) {
@@ -97,6 +125,17 @@ public:
         }
         return ost;
     }
+    int getMenuLen() {
+        return this->menuLength;
+    }
+    const MenuItem*& getMenu() {
+    }
+    bool isInMenu(string itemName) {
+        if (this->menuItems == nullptr) return false;
+        else {
+
+        }
+    }
 };
 
 class Stock {
@@ -105,12 +144,18 @@ private:
     int ingredientsLen = 0;
 public:
     void push(Ingredient ing) {
+        if (this->ingredients != nullptr && this->ingredientsLen != 0) {
+            for (int i = 0; i < this->ingredientsLen; i++) {
+                if (strcmp(this->ingredients[i].getName(), ing.getName()) == 0) {
+                    this->ingredients[i].
+                }
+            }
+        }
         Ingredient* newIng = new Ingredient[ingredientsLen + 1];
         for (int i = 0; i < ingredientsLen; i++) {
             newIng[i] = this->ingredients[i];
         }
         newIng[ingredientsLen] = ing;
-
     }
 };
 
@@ -118,6 +163,17 @@ class Order {
 private:
     MenuItem* currentlyOrdered = nullptr;
     int currentOrderLen = 0;
+public:
+    void addToOrder(MenuItem menuItem, int itemQty) {
+        // user input needs to be sanitized
+        // qty should not be negative
+        // the name of the item should be in the menu
+        MenuItem* newOrder = new MenuItem[currentOrderLen + 1];
+        for (int i = 0; i < currentOrderLen; i++) {
+            newOrder[i] = currentlyOrdered[i];
+        }
+        newOrder[currentOrderLen] = menuItem;
+    }
 };
 
 string Parser::cmdOptions = "\nWelcome!\nYou can look at the menu using \"menu\".\nCheck out our currently available ingredients using \"stock\".\nAdd items to your order with \"order add <item name> <item quanitity>\".\nSee your current order using \"order show\".\nShow this screen again by typing the \"help\" command.\nYou can leave the restaurant using the \"quit\" command.\n\n";
