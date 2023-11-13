ad7cf96f5ec49f2c8b5218d94cafa1ae9f2e8a58
faun3 <antonioculcea@gmail.com>
2023-11-13 18:09:15 +0200

    implement == overload

--- again.cpp
+++ again.cpp
@@ -61,6 +61,18 @@ public:
         }
         return out;
     }
+    // objects with null names ALWAYS compare to false
+    // but they don't compare false against themselves
+    friend bool operator==(const Ingredient& left, const Ingredient& right) {
+        if (&left != &right) {
+        if (left.name != nullptr && right.name != nullptr) {
+            if (strcmp(left.name, right.name) == 0 && left.qty == right.qty) return true;
+            else return false;
+        }
+        else return false;
+        }
+        else return true;
+    }
     const char* getName() {
         return name;
     }
@@ -513,10 +525,13 @@ int main()
     Ingredient i6("olives", 15);
     Ingredient i7("pineapple", 10);
     Ingredient i8("nothing", 0);
+    Ingredient i9("nothing", 0);
     Ingredient i3;
-    if (i1 > i2) cout << "ok\n";
-    if (i2 > i1) cout << "bad\n";
-    if (i1 > i3) cout << "ok\n";
+    if (i1 == i1) cout << "ok\n";
+    // if (i1 == i2) cout << "bad\n";
+    // if (i1 == i3) cout << "bad\n";
+    if (i3 == i3) cout << "bad\n";
+    if (i8 == i9) cout << "ok\n";
     
     Ingredient si1("flour", 1000);
     Ingredient si2("tomatoes", 500);
